7bcbd651747eaf0395d31c72daff3ab25bff4153
Manoj Kumar Patra <patra.manoj0@gmail.com>
2018-10-13 20:12:08 +0530

    Send only call log

--- routes/ctm.js
+++ routes/ctm.js
@@ -6,9 +6,6 @@ var bodyParser = require('body-parser');
 
 router.route('/')
 .post((req, res) => {
-    // fetch('http://18.218.135.95:8000/api/reports/ctm/callback')
-    // .then(d => console.log(d));
-    console.log(req);
     console.log(req.body);
     res.status(200).json({
         data: req.body
