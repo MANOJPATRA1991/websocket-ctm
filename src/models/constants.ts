da38199e3c742f05637ba2c0958ef28981eaf973
Manoj Kumar Patra <patra.manoj0@gmail.com>
2018-10-15 00:00:12 +0530

    Add user handling for CTM

---
+++ src/models/constants.ts
@@ -0,0 +1,5 @@
+export enum Constants {
+    account_id = '177289',
+    account_username = 'patra.manoj0@gmail.com',
+    account_password = 'Manoj@1991'
+}
--- src/routes/ctm.ts
+++ src/routes/ctm.ts
@@ -1,5 +1,8 @@
+import { Constants } from './../models/constants';
+import { Message } from './../../../socket-client/src/app/models/message';
 import { NextFunction, Request, Response, Router } from "express";
 import * as SocketServer from 'ws';
+let base64 = require('base-64');
 
 export class CTMRoute {
     constructor() { }
@@ -12,7 +15,30 @@ export class CTMRoute {
             console.log(users);
 
             if (users[call_log.agent.email]) {
-                users[call_log.agent.email].send(JSON.stringify(call_log));
+                users[call_log.agent.email].send(new Message(JSON.stringify(call_log), 'CTM', false));
+            } else {
+                let url = `https://api.calltrackingmetrics.com/api/v1/accounts/${Constants.account_id}/calls/${call_log.id}/endcall`;
+                let username = Constants.account_username;
+                let password = Constants.account_password;
+
+                let headers = new Headers();
+
+                headers.set('Authorization', 'Basic ' + base64.encode(username + ":" + password));
+                headers.append("Content-Type", "application/json; charset=utf-8");
+
+                fetch(url, {
+                    method: "POST",
+                    mode: "cors",
+                    cache: "no-cache",
+                    credentials: "same-origin",
+                    headers: headers,
+                    redirect: "follow",
+                    referrer: "no-referrer",
+                    body: JSON.stringify({})
+                })
+                .then(response => {
+                    console.log(response.json());
+                });
             }
 
             res.status(200).json({
